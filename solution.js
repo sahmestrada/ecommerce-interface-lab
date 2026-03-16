23989f53750519c0b3f6b92ff0974b061390e432
sahmestrada <sahmestrada@gmail.com>
2026-03-16 17:06:39 +0800

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,12 @@
+function safeDivide(a, b) {
+  try {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero");
+    }
+    return a / b;
+  } catch (error) {
+    return error.message;
+  } finally {
+    console.log("Operation attempted");
+  }
+}
