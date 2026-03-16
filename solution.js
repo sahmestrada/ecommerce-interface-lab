dabb6257cb1d860e0aff2a1dc5871730461ffbe7
sahmestrada <sahmestrada@gmail.com>
2026-03-16 15:55:52 +0800

    Update solution.js

---
+++ solution.js
@@ -0,0 +1,13 @@
+function checkVariable(input) {
+  if (input === null) return "object";
+  
+  switch (typeof input) {
+    case "string": return "string";
+    case "number": return "number";
+    case "boolean": return "boolean";
+    case "bigint": return "bigint";
+    case "undefined": return "undefined";
+    case "object": return "object";
+    default: return "unknown type";
+  }
+}
